910b1ccf6135d6c496d7b408238b621385dfc4d0
MUSTAFA YENAL <202803018@ogr.cbu.edu.tr>
2021-03-31 13:17:57 +0300

    Add project files.

---
+++ Class_calismasi/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Class_calismasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.UrunAdi = "Çalışma Masası";
+            urun1.UrunFiyatı = 174.99;
+            urun1.StokMiktarı = 13;
+
+            Urun urun2 = new Urun();
+            urun2.UrunAdi = "Koltuk Takımı";
+            urun2.UrunFiyatı = 1499.99;
+            urun2.StokMiktarı = 5;
+
+            Urun urun3 = new Urun();
+            urun3.UrunAdi = "Ev Telefonu";
+            urun3.UrunFiyatı = 499.99;
+            urun3.StokMiktarı = 8;
+
+
+            Urun[] urunler = new Urun[] { urun1, urun2, urun3 };
+            //listeleme yöntemini foreach,for,while döngüleriyle yapılacak!!!
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.UrunAdi + " - " 
+                    + urun.UrunFiyatı + " TL - " 
+                    + urun.StokMiktarı + " adet kalmıştır ");
+            }
+            Console.WriteLine("foreach dongüsü bitti\n\n");
+            for(int i=0;i<urunler.Length;i++)
+            {
+                Console.WriteLine(urunler[i].UrunAdi + " - " 
+                    + urunler[i].UrunFiyatı + " TL - "
+                    + urunler[i].StokMiktarı + " adet kalmıştır ");
+            }
+            Console.WriteLine("for döngüsü bitti\n\n");
+
+            int j = 0;
+            while (j<urunler.Length)
+            {
+               Console.WriteLine(urunler[j].UrunAdi + " - "
+                   + urunler[j].UrunFiyatı + " TL - "
+                   + urunler[j].StokMiktarı + " adet kalmıştır ");  
+
+                j++;
+            }
+        }
+    }
+}
---
+++ Class_calismasi/Urun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_calismasi
+{
+    class Urun
+    {
+        public string UrunAdi { get; set; }
+        public double UrunFiyatı { get; set; }
+        public int StokMiktarı { get; set; }
+    }
+}
